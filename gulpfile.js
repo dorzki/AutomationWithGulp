4bb0625b704622f0f9d30cc49582db46e192c9ba
Dor Zuberi <webmaster@dorzki.co.il>
2018-07-24 12:41:35 +0300

    Part 3 files.

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,20 @@
 
 // Dependencies
 var gulp = require( 'gulp' );
+var minifyCSS = require( 'gulp-clean-css' );
+var concat = require( 'gulp-concat' );
+
+// Variables
+var CSS_SRC = '__src/css/*.css';
+var CSS_DIST = 'assets/css';
 
 // Tasks
-gulp.task( 'hello', function () {
+gulp.task( 'minify-css', function() {
 
-  console.log( 'Hello World!' );
+  return gulp
+          .src( CSS_SRC )
+          .pipe( minifyCSS() )
+          .pipe( concat( 'styles.min.css' ) )
+          .pipe( gulp.dest( CSS_DIST ) );
 
 } );
