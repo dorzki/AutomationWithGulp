d1a6433bf67c7ecb5d8acbd7a8b2ce27e8f442d1
Dor Zuberi <webmaster@dorzki.co.il>
2018-07-26 18:07:39 +0300

    Part 4 files.

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,37 @@
 
 // Dependencies
 var gulp = require( 'gulp' );
+var scss = require( 'gulp-sass' );
+var less = require( 'gulp-less' );
+var concat = require( 'gulp-concat' );
+var minifyCSS = require( 'gulp-clean-css' );
+
+// Variables
+var SCSS_SRC = '__src/scss/**/*.scss';
+var SCSS_DIST = 'assets/css';
+var LESS_SRC = '__src/less/**/*.less';
+var LESS_DIST = 'assets/css';
 
 // Tasks
-gulp.task( 'hello', function () {
+gulp.task( 'build-scss', function() {
 
-  console.log( 'Hello World!' );
+  return gulp
+          .src( SCSS_SRC )
+          .pipe( scss( {
+            outputStyle: 'compressed'
+          } ) )
+          .pipe( concat( 'styles.min.css' ) )
+          .pipe( gulp.dest( SCSS_DIST ) );
+
+} );
+
+gulp.task( 'build-less', function() {
+
+  return gulp
+          .src( LESS_SRC )
+          .pipe( less() )
+          .pipe( minifyCSS() )
+          .pipe( concat( 'styles.min.css' ) )
+          .pipe( gulp.dest( LESS_DIST ) );
 
 } );
