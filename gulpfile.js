0c681da2a0d4074a11c39a8944510709e8bd4314
Dor Zuberi <webmaster@dorzki.co.il>
2019-02-12 21:35:21 +0200

    Part 9 files.

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,22 @@
  * Automation with Gulp
  */
 
-// Dependencies
+	// Dependencies
 var gulp = require( 'gulp' );
+var scss = require( 'gulp-sass' );
+var combine_mq = require( 'gulp-group-css-media-queries' );
+
+// Variables
+var SCSS_SRC = '__src/scss/**/*.scss';
+var SCSS_DIST = 'assets/css';
 
 // Tasks
-gulp.task( 'hello', function () {
+gulp.task( 'build-scss', function () {
 
-  console.log( 'Hello World!' );
+	return gulp
+		.src( SCSS_SRC )
+		.pipe( scss() )
+		.pipe( combine_mq() )
+		.pipe( gulp.dest( SCSS_DIST ) );
 
 } );
