d95dc530bd5e20bedfc72180da4ba45de7922afe
Dor Zuberi <webmaster@dorzki.co.il>
2019-03-13 02:51:37 +0200

    Part 10 files.

---
+++ assets/js/scripts.js
@@ -0,0 +1,5 @@
+( function( $ ) {
+
+	// Something...
+
+} )( jQuery );
--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,20 @@
 
 // Dependencies
 var gulp = require( 'gulp' );
+var inject = require( 'gulp-inject' );
+
+// Variables
+var CSS_SRC = [ 'assets/css/vendor/*.css', 'assets/css/*.css' ];
+var JS_SRC = [ 'assets/js/vendor/*.js', 'assets/js/*.js' ];
+var HTML_FILES = '*.html';
 
 // Tasks
-gulp.task( 'hello', function () {
+gulp.task( 'inject-assets', function () {
 
-  console.log( 'Hello World!' );
+	return gulp
+		.src( HTML_FILES )
+		.pipe( inject( gulp.src( CSS_SRC, { read: false } ) ) )
+		.pipe( inject( gulp.src( JS_SRC, { read: false } ) ) )
+		.pipe( gulp.dest( './' ) );
 
 } );
